d80504884432fbca9efdc9b2cc4cfd805961802c
Stepan Severylov <stepan.severylov2008@gmail.com>
2023-03-20 19:36:33 +0200

    add css and responsive design

--- src/components/profile/CharacterItem.js
+++ src/components/profile/CharacterItem.js
@@ -1,72 +1,75 @@
+import './CharacterItem.css'
+
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import Service from "../../services/get-service";
 
 function CharactersItem() {
-    const [data, setData] = useState({
-        name: "ERROR 404",
-        gender: "Unknown",
-        status: "Unknown",
-        species: "Unknown", 
-        origin: "Unknown",
-        type: "Unknown",
-        image: "Unknown"
-    })
+    const [data, setData] = useState();
     
-    let params = useParams()
-    let navigate = useNavigate()
+    let params = useParams();
+    let navigate = useNavigate();
 
     useEffect(() => {
         Service.getCharacter(`/${params.userId}`)
-            .then(res => setData(res.data))
-    }, [params.userId])
+            .then(res => setData(res.data));
+    }, [params.userId]);
 
-    const {name, gender, status, species, origin, type, image} = data;
+    
+    if (data) {
+        
+        const {name, gender, status, species, origin, type, image} = data;
 
-    return(
-        <div>
-            <button onClick={() => {navigate('/')}}>Go back</button>
-            <div>
-                <img src={image} alt={name}/>
-                <h1>{name}</h1>
+        return(
+            <div className='item'>
+                <div className='button'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-left" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+                    </svg>
+                    <button onClick={() => {navigate('/')}}>Go back</button>
+                </div>
+                <div className='image'>
+                    <img src={image} alt={name}/>
+                    <h1>{name}</h1>
+                    <h2>Information</h2>
+                </div>
+                <div className='info'>
+                    <ul>
+                        <li>
+                            <div className='gender'>
+                                <p>Gender</p>
+                                <p>{gender}</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className='status'>
+                                <p>Status</p>
+                                <p>{status}</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className='species'>
+                                <p>Species</p>
+                                <p>{species}</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className='origin'>
+                                <p>Origin</p>
+                                <p>{origin.name}</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className='type'>
+                                <p>Type</p>
+                                <p>{type === "" ? "unknown" : type}</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div>
-                <h2>Information</h2>
-                <ul>
-                    <li>
-                        <div>
-                            <p>Gender</p>
-                            <p>{gender}</p>
-                        </div>
-                    </li>
-                    <li>
-                        <div>
-                            <p>Status</p>
-                            <p>{status}</p>
-                        </div>
-                    </li>
-                    <li>
-                        <div>
-                            <p>Species</p>
-                            <p>{species}</p>
-                        </div>
-                    </li>
-                    <li>
-                        <div>
-                            <p>Origin</p>
-                            <p>{origin.name}</p>
-                        </div>
-                    </li>
-                    <li>
-                        <div>
-                            <p>Type</p>
-                            <p>{type === "" ? "Unknown" : type}</p>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default CharactersItem;
